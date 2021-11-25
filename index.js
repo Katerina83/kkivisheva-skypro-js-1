8d909a163d3880a8303f0c1f76e352f1bab3bed1
Katerina83 <Katerina83@users.noreply.github.com>
2021-11-25 17:00:12 +0000

    Deploying to gh-pages from @ Katerina83/kkivisheva-skypro-js-1@3a229d921b8786330de4f64844c13dd1d00f0616 🚀

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-"use strict";
+//hw2
+//Задание #1 (функция генерирующая случайное число)
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (min >= max) {
+    console.log('Пожалуйста, введите другое число. Числа либо равны, либо минимальное число больше максимального');
+  } else {
+  return 'случайное число равно ' + Math.floor(Math.random() * (max - min)) + min;
+}
+}
+// eslint-disable-next-line
+console.log(getRandomInt(3,10))
+console.log(getRandomInt(5,10))
+console.log(getRandomInt(1,90))
 
-let product = "Диван RONALD";
-let price = 45000;
-alert(product + ", цена " + price + " рублей.");
+//Задание #2 ()
+
